59f799313c32648791b41614e0d0c9d8266c816f
Cedric Shih <cedric.shih@gmail.com>
2022-07-03 21:11:44 +0800

    wip: v2

--- controller/milestone.go
+++ controller/milestone.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"text/template"
 )
 
@@ -19,6 +20,84 @@ type MilestoneController struct {
 }
 
 func (c *MilestoneController) Handler(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.Header.Get("Content-Type"), "multipart/form-data") {
+		c.handleForm(w, r)
+	} else {
+		c.handlePost(w, r)
+	}
+}
+
+func (c *MilestoneController) handlePost(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	distance, err := strconv.ParseFloat(vars.Get("distance"), 64)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Invalid distance: %s", err.Error()), 400)
+		return
+	}
+	log, err := gpx.Parse(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	tmpl, err := template.New("").Parse(vars.Get("name-template"))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to parse template: %s", err.Error()), 400)
+		return
+	}
+	marker := &milestone.Marker{
+		Distance:     distance,
+		NameTemplate: tmpl,
+		Service:      c.Service,
+		Symbol:       "Milestone",
+	}
+	if _, ok := vars["reverse"]; ok {
+		marker.Reverse = true
+	}
+	format := vars.Get("format")
+	switch format {
+	case "gpx":
+		err = marker.MarkToGPX(log)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to mark GPX: %s", err.Error()), 500)
+			return
+		}
+		writer := &gpx.Writer{
+			Creator: c.GPXCreator,
+			Writer:  w,
+		}
+		w.Header().Set("Content-Type", "application/gpx+xml")
+		err = writer.Write(log)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to write GPX: %s", err.Error()), 500)
+			return
+		}
+		return
+	case "csv":
+		records := [][]string{
+			{r.FormValue("csv-name"), r.FormValue("csv-latitude"), r.FormValue("csv-longitude"), r.FormValue("csv-elevation")},
+		}
+		records, err = marker.MarkToCSV(records, log)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to create CSV: %s", err.Error()), 500)
+			return
+		}
+		w.Header().Set("Content-Type", "text/csv")
+		writer := csv.NewWriter(w)
+		for _, record := range records {
+			if err := writer.Write(record); err != nil {
+				http.Error(w, fmt.Sprintf("Failed to write CSV: %s", err.Error()), 500)
+				return
+			}
+		}
+		writer.Flush()
+		return
+	default:
+		http.Error(w, fmt.Sprintf("Unknown format: %s", format), 400)
+		return
+	}
+}
+
+func (c *MilestoneController) handleForm(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseMultipartForm(10 * 1048576)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
